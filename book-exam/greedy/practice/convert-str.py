870702a673d9feb643392eef53fe096d0dbc4a8b
hyeilly <dev.jiine@gmail.com>
2024-01-30 08:56:19 +0900

    add guild

--- book-exam/greedy/practice/convert-str.py
+++
@@ -1,9 +0,0 @@
-# 연속된 하나 이상의 숫자를 잡고 모두 뒤집기
-s = '0001100'
-res = ''
-count = 0
-type = s[0]
-for i in range(1, len(s)):
-    if type != s[i]:
-        print(s[:i])
-        break
--- book-exam/greedy/practice/guild.py
+++ book-exam/greedy/practice/guild.py
@@ -1,8 +1,19 @@
-# 모험가 길드
-# N명 모험가 대상으로 '공포도'측정 => 높은 모험가는 쉽게 공포를 느껴 위험 상황에서 제대로 대처 X
-# 공포도가 X인 모험가는 반드시 X명 이상 구성한 모험가 그룹 참여
-n = int(input())
-scary = list(map(int, input().split()))
-scary.sort()
-print(scary)
+# 모험가 N 명
+# '공포도' 측정
+# 그룹을 안전하게 구성하고자 공포도가 X인 모험가는 반드시 X명 이상으로 구성
+N = 5
+scary = [2, 3, 1, 2, 2]
+# N = int(input())
+# scary = map(int, input().split())
 
+scary.sort()
+cnt = 0
+max_v = 0
+result = 0
+
+for i in scary:
+    cnt += 1
+    if cnt >= i:
+        result += 1
+        cnt = 0
+print(result)
