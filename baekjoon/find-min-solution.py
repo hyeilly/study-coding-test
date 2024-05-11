644ffe8bfd76124f13bc98d5b78bfbf07ef3e67e
hyeilly <dev.jiine@gmail.com>
2024-05-11 13:35:30 +0900

    deque 사용한 정답 문제풀이

--- baekjoon/find-min-solution.py
+++ baekjoon/find-min-solution.py
@@ -3,3 +3,25 @@
 # 데이터를 양쪽으로 넣었다 뺐다
 # 1. 최소값 가능성이 없는 데이터 삭제
 # 2. 윈도우 크기 밖으로 넘어간 데이터 삭제
+# N, L = 12 3
+# now = 1 5 2 3 6 2 3 7 3 5 2 6
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, L = map(int, input().split())
+mydeque = deque()
+now = list(map(int, input().split()))
+
+for i in range(N):
+    # 나보다 큰 데이터 삭제
+    # 나의 deque 가장 끝에 있는 값이 더 크면
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    # 슬라이딩 윈도우를 벗어난 데이터 삭제
+    # 내 deque의 맨 첫번째 인덱스가 현재 들어온 데이터와 비교
+    if mydeque[0][1] <= i-L:
+        mydeque.popleft()
+
+    print(mydeque[0][0], end=' ')
