a0c0d85715a17ffe99a537290c037818e43177e8
hyeilly <dev.jiine@gmail.com>
2024-05-11 13:14:22 +0900

    입력 추가 수정

--- baekjoon/find-min.py
+++ baekjoon/find-min.py
@@ -5,16 +5,23 @@
 # 최솟값을 Di 라고 할 때 D에 저장된 수를 출력하는 프로그램
 # i<=0인 Ai 는 무시하고 D를 구해야함
 
-N, L = 12, 3
-D = [1, 5, 2, 3, 6, 2, 3, 7, 3, 5, 2, 6]
+N, L = map(int, input().split())
+D = list(map(int, input().split()))
+
+# N, L = 12, 3
+# D = [1, 5, 2, 3, 6, 2, 3, 7, 3, 5, 2, 6]
 answer_array = []
+
 
 for i in range(N):
     check_array = []
-    window_1 = 0 if i - 1 < 0 else i - 1
-    window_2 = 0 if i - 2 < 0 else i - 2
-    check_array = [D[i], D[window_1], D[window_2]]
-
+    window = []
+    window.append(i)
+    for l in range(1, L):
+        window_data = 0 if i - l < 0 else i - l
+        window.append(window_data)
+    for w in window:
+        check_array.append(D[w])
     answer_array.append(str(min(check_array)))
 
 print(' '.join(answer_array))
