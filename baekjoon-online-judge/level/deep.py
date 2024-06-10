68aaa7d0752a7a2cfac429306fecdaf70ab75901
hyeilly <dev.jiine@gmail.com>
2024-06-10 20:35:52 +0900

    심화 문제 추가

--- baekjoon-online-judge/level/deep.py
+++ baekjoon-online-judge/level/deep.py
@@ -15,3 +15,33 @@ def chess_calc():
     for r in result:
         print(r, end=' ')
 
+def palindrome():
+    S = list(input())
+    a = ''.join(S)
+    b = ''.join(S[::-1])
+    if a == b:
+        print(1)
+    else:
+        print(0)
+
+def word_study():
+    S = list(input().lower())
+    result = {}
+    for i in S:
+        if i not in result:
+            result[i] = 0
+        else:
+            result[i] += 1
+    li = [k for k, v in result.items() if max(result.values()) == v]
+    if len(li) > 1:
+        print("?")
+    else:
+        print(li[0].upper())
+# N = int(input())
+# numb = 2 * N - 1
+# max_result = ''
+# for k in range(numb):
+#     result = ''
+#     for j in range(numb):
+#         result += '*'
+#     print(result)
