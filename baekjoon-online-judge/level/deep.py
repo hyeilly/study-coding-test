d99adc32f4849e6215cb48707dee9a489544ff49
hyeilly <dev.jiine@gmail.com>
2024-06-11 15:37:53 +0900

    심화, 2차원 행렬 문제 풀이

--- baekjoon-online-judge/level/deep.py
+++ baekjoon-online-judge/level/deep.py
@@ -37,11 +37,57 @@ def word_study():
         print("?")
     else:
         print(li[0].upper())
+
+
 # N = int(input())
-# numb = 2 * N - 1
-# max_result = ''
-# for k in range(numb):
-#     result = ''
-#     for j in range(numb):
-#         result += '*'
-#     print(result)
+# S = 2 * N - 1
+# S_h = int(S / 2)
+# S_half = int(S / 2)
+# t = 1
+# for i in range(S):
+#     S_h -= 1
+#     star = ''
+#     for j in range(abs(S_h+1), 0, -1):
+#         star += ' '
+#     if i > S_half:
+#         print(i)
+#
+#     else:
+#         print(i)
+#
+#
+#     print(star)
+    # for k in range(1, S):
+    #     print(k + (k - 1))
+
+
+
+    # print(i * 3 - 1)
+
+
+score = {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D": 1.0,
+    "F": 0.0
+}
+all = 0
+cnt = 0
+while True:
+    try:
+        subject = list(input().split(" "))
+        grade = subject[2]  # 과목평점
+        if grade != 'P':
+            hj = float(subject[1]) # 학점
+
+            if grade in score:
+                all += hj
+                cnt += hj * score[grade]
+    except:
+        print(cnt/all)
+        break
--- baekjoon-online-judge/level/dim2-arr-practice.py
+++ baekjoon-online-judge/level/dim2-arr-practice.py
@@ -1,16 +1,40 @@
-n, m = map(int, input().split())
-n_a, m_a = [], []
-for _ in range(n):
-    n_arr = list(map(int, input().split()))
-    n_a.append(n_arr)
-for _ in range(m):
-    m_arr = list(map(int, input().split()))
-    m_a.append(m_arr)
-for i in range(n):
-    res = []
-    for k in range(len(n_a[i])):
-        res.append(n_a[i][k] + m_a[i][k])
-    for r in res:
-        print(r, end=' ')
-    if i < n - 1:
+def procession_sum():
+    n, m = map(int, input().split())
+    n_a, m_a = [], []
+    for _ in range(n):
+        n_arr = list(map(int, input().split()))
+        n_a.append(n_arr)
+    for _ in range(n):
+        m_arr = list(map(int, input().split()))
+        m_a.append(m_arr)
+
+    for i in range(n):
+        for k in range(len(n_a[i])):
+            print(n_a[i][k] + m_a[i][k], end=' ')
         print()
+def procession_max():
+    arr = []
+    for _ in range(9):
+        li = list(map(int, input().split()))
+        arr.append(li)
+
+    temp = 0
+    temp_row = 0
+    temp_column = 0
+
+    for (adx, a) in enumerate(arr):
+        if temp <= max(a):
+            temp = max(a)
+            temp_row = adx + 1
+            temp_column = a.index(temp) + 1
+    print(temp)
+    print(temp_row, temp_column)
+
+arr = []
+for _ in range(5):
+    input_data = list(input())
+    arr.append(input_data)
+
+result = ''
+
+print(len(arr))
